ce759d9f4b26d363afff601b1de71bb219cb238d
Gagah007 <gagahsp4@gmail.com>
2020-11-16 20:24:05 +0700

    menambahkan function hamburgerMenu

---
+++ script.js
@@ -0,0 +1,13 @@
+function learnMore(){
+    document.getElementById("learn").addEventListener(alert("Mohon maaf, saat ini masih dalam proses pengembangan"));
+}
+
+function hamburgerMenu(){
+    var x = document.getElementById("menu");
+    if (x.style.display === "flex"){
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+
+}
